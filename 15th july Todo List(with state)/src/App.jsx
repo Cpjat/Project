6fba36e824c910bb212b825e1627863d08d6508f
Chandra Prakash <cpjat141@gmail.com>
2025-07-23 08:55:25 +0530

    Add files via upload

---
+++ 15th july Todo List(with state)/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const handleAdd = () => {
+    if (task.trim() !== '') {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
+
+  return (
+    <div className="app-container">
+      <h1>Simple To-Do List</h1>
+      <div className="input-group">
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Enter a task"
+        />
+        <button onClick={handleAdd}>Add</button>
+      </div>
+
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>{t}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
